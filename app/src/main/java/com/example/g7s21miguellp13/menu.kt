4b548c30e6c7ddf8c3c58b9628f708380b18b36b
Miguel Friky <dnmiguel.friky@gmail.com>
2021-12-28 18:05:04 -0600

    2

--- app/src/main/java/com/example/g7s21miguellp13/menu.kt
+++ app/src/main/java/com/example/g7s21miguellp13/menu.kt
@@ -2,10 +2,35 @@ package com.example.g7s21miguellp13
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.g7s21miguellp13.databinding.ActivityMenuBinding
+
 
 class menu : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_menu)
+        val binding = ActivityMenuBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
+        binding.btnListar.setOnClickListener {}
+
+
+        binding.btnInsertar.setOnClickListener {}
+
+
+        binding.btnActualizar.setOnClickListener {}
+
+
+        binding.btnEliminar.setOnClickListener {}
+
+
+        binding.btnSalir.setOnClickListener {}
+
+
+
+
+
+
     }
 }
