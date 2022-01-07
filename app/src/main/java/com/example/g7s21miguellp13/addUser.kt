c2d81e1e942861d670474e9696a0de32582711de
Miguel Friky <dnmiguel.friky@gmail.com>
2022-01-07 12:29:33 -0600

    add2

--- app/src/main/java/com/example/g7s21miguellp13/addUser.kt
+++ app/src/main/java/com/example/g7s21miguellp13/addUser.kt
@@ -2,10 +2,35 @@ package com.example.g7s21miguellp13
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.g7s21miguellp13.databinding.ActivityAddUserBinding
+import com.example.g7s21miguellp13.AdminSQLiteOpenHelper
 
 class addUser : AppCompatActivity() {
+   //lateinit var dbUsers: AdminSQLiteOpenHelper
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_user)
+        val binding = ActivityAddUserBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        var dbUsers: AdminSQLiteOpenHelper = AdminSQLiteOpenHelper(this)
+
+        binding.button.setOnClickListener {
+            if(binding.ageText.text.isNotBlank() && binding.nameText.text.isNotBlank() && binding.mailText.text.isNotBlank()){
+
+                dbUsers.addDate(binding.nameText.text.toString(), binding.ageText.text.toString().toInt(), binding.mailText.text.toString())
+                binding.ageText.text.clear()
+                binding.nameText.text.clear()
+                binding.mailText.text.clear()
+
+                Toast.makeText(this,"Nuevo usuario agregado",Toast.LENGTH_LONG)
+            }
+            else{
+                Toast.makeText(this,"Llena todos los campos",Toast.LENGTH_LONG)
+            }
+        }
+
+
+
     }
 }
