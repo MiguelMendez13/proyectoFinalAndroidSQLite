b4067a315694c671b8b2558532ed47b32ad714e0
Miguel Friky <dnmiguel.friky@gmail.com>
2022-01-07 12:37:22 -0600

    22

--- app/src/main/java/com/example/g7s21miguellp13/MainActivity.kt
+++ app/src/main/java/com/example/g7s21miguellp13/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.g7s21miguellp13
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +8,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val menu = Intent(this, menu::class.java)
+        startActivity(menu)
     }
 }
