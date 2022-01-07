c51a15547e622456ffdd5f114bcd8d84f4710a1b
Miguel Friky <dnmiguel.friky@gmail.com>
2022-01-07 13:11:10 -0600

    listElements

---
+++ app/src/main/java/com/example/g7s21miguellp13/listElements.kt
@@ -0,0 +1,11 @@
+package com.example.g7s21miguellp13
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class listElements : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_elements)
+    }
+}
