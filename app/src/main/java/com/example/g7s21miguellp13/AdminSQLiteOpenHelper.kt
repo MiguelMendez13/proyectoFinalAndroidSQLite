0d06768058b8e2483796e25051d8a04531bcbedc
Miguel Friky <dnmiguel.friky@gmail.com>
2022-01-07 11:36:38 -0600

    Update AdminSQLiteOpenHelper.kt

--- app/src/main/java/com/example/g7s21miguellp13/AdminSQLiteOpenHelper.kt
+++ app/src/main/java/com/example/g7s21miguellp13/AdminSQLiteOpenHelper.kt
@@ -1,5 +1,6 @@
 package com.example.g7s21miguellp13
 
+import android.content.ContentValues
 import android.content.Context
 import android.database.sqlite.SQLiteOpenHelper
 import android.database.sqlite.SQLiteDatabase
@@ -9,10 +10,27 @@ import android.database.sqlite.SQLiteDatabase.CursorFactory
 class AdminSQLiteOpenHelper (context: Context,) : SQLiteOpenHelper(context, "Usuarios.db", null, 1) {
 
     override fun onCreate(db: SQLiteDatabase) {
-        db.execSQL("create table usuarios(id int primary key autoincrement, Nombre text, Edad int, Correo text)")
+        db.execSQL("create table users(id int primary key autoincrement, Name text, Age int, Email text)")
     }
 
     override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
 
     }
+
+
+
+    fun addDate(Name: String, Age: Int, Email: String){
+        val dates= ContentValues()
+        dates.put("name",Name)
+        dates.put("age",Age)
+        dates.put("email",Email)
+
+
+        val db = this.writableDatabase
+        db.insert("users",null,dates)
+        db.close()
+
+    }
+
 }
+
