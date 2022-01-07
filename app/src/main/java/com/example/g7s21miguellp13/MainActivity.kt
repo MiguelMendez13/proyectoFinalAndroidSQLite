18136f6509c502af67baec2f0ca6f30a24961021
Miguel Friky <dnmiguel.friky@gmail.com>
2022-01-07 13:00:37 -0600

    11

--- app/src/main/java/com/example/g7s21miguellp13/MainActivity.kt
+++ app/src/main/java/com/example/g7s21miguellp13/MainActivity.kt
@@ -8,8 +8,20 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        abrir()
 
-        val menu = Intent(this, menu::class.java)
-        startActivity(menu)
+    }
+    fun abrir(){
+        val thread= Thread(Runnable{
+            Thread.sleep(5000)
+            runOnUiThread {
+                val menu = Intent(this, menu::class.java)
+                startActivity(menu)
+            }
+        })
+
+        thread.start()
+
+
     }
 }
