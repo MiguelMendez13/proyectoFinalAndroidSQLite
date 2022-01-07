a3a370e86b8208ccdebaeeb6e55919a277bc01cc
Miguel Friky <dnmiguel.friky@gmail.com>
2022-01-07 11:24:51 -0600

    Update AdminSQLiteOpenHelper.kt

--- app/src/main/java/com/example/g7s21miguellp13/AdminSQLiteOpenHelper.kt
+++ app/src/main/java/com/example/g7s21miguellp13/AdminSQLiteOpenHelper.kt
@@ -6,10 +6,10 @@ import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteDatabase.CursorFactory
 
 
-class AdminSQLiteOpenHelper (context: Context, name: String, factory: CursorFactory?, version: Int) : SQLiteOpenHelper(context, name, factory, version) {
+class AdminSQLiteOpenHelper (context: Context,) : SQLiteOpenHelper(context, "Usuarios.db", null, 1) {
 
     override fun onCreate(db: SQLiteDatabase) {
-        db.execSQL("create table articulos(codigo int primary key, descripcion text, precio real)")
+        db.execSQL("create table usuarios(id int primary key autoincrement, Nombre text, Edad int, Correo text)")
     }
 
     override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
